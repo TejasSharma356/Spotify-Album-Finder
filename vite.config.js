5ec5b80bf798550667be97027e540d3b24adaf67
TejasSharma356 <you@example.com>
2025-08-21 23:03:54 +0530

    fix: vite config for vercel

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,5 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [react()],
-  base: '/spotify-album-finder/', // use your repo name here
+  plugins: [react()]
 })
-// This configuration file sets up Vite for a React project
-// with the React plugin and specifies the base path for deployment.
