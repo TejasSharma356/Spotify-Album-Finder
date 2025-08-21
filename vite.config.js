fa478962511379c1c623ced53dfca297f22e2389
TejasSharma356 <you@example.com>
2025-08-21 22:37:38 +0530

    first commit

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/spotify-album-finder/', // use your repo name here
 })
+// This configuration file sets up Vite for a React project
+// with the React plugin and specifies the base path for deployment.
